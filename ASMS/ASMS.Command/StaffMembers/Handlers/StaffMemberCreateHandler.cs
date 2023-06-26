7d305c33df3565fd25abebd5e402ad1dd3766d3b
Eduardo Citelli <eduardo.citelli@hotmail.com>
2023-06-26 09:48:34 -0300

    fix staff member create handler

--- ASMS/ASMS.Command/StaffMembers/Handlers/StaffMemberCreateHandler.cs
+++ ASMS/ASMS.Command/StaffMembers/Handlers/StaffMemberCreateHandler.cs
@@ -1,6 +1,7 @@
 ﻿using ASMS.Command.StaffMembers.Commands;
 using ASMS.DTOs.StaffMembers;
 using ASMS.Infrastructure;
+using ASMS.Infrastructure.Security;
 using ASMS.Services.Abstractions;
 using MediatR;
 
@@ -9,14 +10,21 @@ namespace ASMS.Command.StaffMembers.Handlers
     public class StaffMemberCreateHandler : IRequestHandler<StaffMemberCreateCommand, BaseApiResponse<StaffMemberSingleDto>>
     {
         private readonly IStaffMemberService _staffMemberService;
+        private readonly IUserService _userService;
 
-        public StaffMemberCreateHandler(IStaffMemberService staffMemberService)
+        public StaffMemberCreateHandler(IStaffMemberService staffMemberService, IUserService userService)
         {
             _staffMemberService = staffMemberService;
+            _userService = userService;
         }
 
         public async Task<BaseApiResponse<StaffMemberSingleDto>> Handle(StaffMemberCreateCommand request, CancellationToken cancellationToken)
         {
+            await _userService.ValidateExistentInfo(request.User.UserName, request.User.Email);
+
+            request.User.Password = request.User.Password.ToHash();
+            request.User.Email = request.User.Email.ToLower();
+
             return await _staffMemberService.CreateAsync(request);
         }
     }
