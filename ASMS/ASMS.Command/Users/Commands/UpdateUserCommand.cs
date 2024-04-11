4220e9235a7f26282dac9a5139b910538ca5ad88
Eduardo Citelli <eduardo.citelli@hotmail.com>
2024-04-11 20:30:05 -0300

    fix

--- ASMS/ASMS.Command/Users/Commands/UpdateUserCommand.cs
+++ ASMS/ASMS.Command/Users/Commands/UpdateUserCommand.cs
@@ -6,6 +6,7 @@ namespace ASMS.Command.Users.Commands
 {
     public class UpdateUserCommand : UserUpdateDto, IRequest<BaseApiResponse<UserBasicDto>>
     {
+        public UpdateUserCommand() { }
         public UpdateUserCommand(UpdateUserCommand request, long id)
         {
             Id = id;
